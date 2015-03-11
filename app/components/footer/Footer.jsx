18679d74053dcd8a019b5d8be071352e86c1a2bf
Develra <michael.drew.aaron@gmail.com>
2015-03-11 11:52:12 -0600

    add jsx stubs and wiring

---
+++ app/components/footer/Footer.jsx
@@ -0,0 +1,7 @@
+class Footer extends React.Component {
+  render() {
+    return (<div class="footer">Footer Placeholder</div>);
+  }
+}
+
+module.exports = Footer;
---
+++ app/components/template-viewer/TemplateViewer.jsx
@@ -0,0 +1,7 @@
+class TemplateViewer extends React.Component {
+  render() {
+    return (<div class="template-viewer"></div>);
+  }
+}
+
+module.exports = TemplateViewer;
