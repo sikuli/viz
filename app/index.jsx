de74abd5cc99b96f6eea9c10aba74837c3fdd4b3
Peyman Mortazavi <pmortazavi@cailmobility.com>
2015-03-27 15:30:50 -0600

    added animation for collapsing

--- app/index.jsx
+++ app/index.jsx
@@ -4,28 +4,33 @@ import DataViewer from "./components/data-viewer/DataViewer.jsx";
 import TemplateViewer from "./components/template-viewer/TemplateViewer.jsx";
 import Visualizer from "./components/visualizer/Visualizer.jsx";
 
-$('.collapser').on('click', function() {
+$('#collapseButton').on('click', function() {
   var dataP = $('#data-viewer');
   var templateV = $('#template-viewer');
   var visualizerW = $("#visualizer");
-  console.log(dataP.css("visibility") == "collapse");
-  if (dataP.css("visibility") == "collapse" && templateV.css("visibility") == "collapse") {
-        visualizerW.switchClass("col-md-12", "col-md-8" , 500, 'swing', function () { 
-            dataP.animate({ opacity: "1.0" }, 500, function () {
-                dataP.css("visibility", "visible").css("display", "block");
-            });
-            templateV.animate({ opacity: "1.0" }, 500, function () {
-                templateV.css("visibility", "visible").css("display", "block");
-            });
-        });
+  var animationDuration = 500;
+  var animationEasing = "swing";
+
+  var setCollapse = function(element, shouldCollapse){
+    if(shouldCollapse){
+      element.addClass("CX");
+    }
+    else{
+      element.removeClass("CX");
+    }
+  };
+
+  if (dataP.hasClass("CX") && templateV.hasClass("CX")) {
+
+    dataP.animate({ "width": "20%"}, animationDuration, animationEasing, setCollapse(dataP, false));
+    templateV.animate({ width: "20%"}, animationDuration, animationEasing, setCollapse(templateV, false));
+    visualizerW.animate({width: "60%"});
+
     } else {
-        setTimeout( function () { visualizerW.switchClass("col-md-8", "col-md-12" , 500) }, 1000);
-        dataP.animate({ opacity: "0.0" }, 500, function () {
-            dataP.css("visibility", "collapse").css("display", "none");
-        });
-        templateV.animate({ opacity: "0.0" }, 500, function () {
-            templateV.css("visibility", "collapse").css("display", "none");
-        });
+
+    dataP.animate({ width: "0%"}, animationDuration, animationEasing, setCollapse(dataP, true));
+    templateV.animate({ width: "0%"}, animationDuration, animationEasing, setCollapse(templateV, true));
+    visualizerW.animate({width: "100%"});
   }
 });
 
