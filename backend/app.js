723dc34cac4abc56493b39ff69d0d56796d23b1a
Peyman Mortazavi <pmortazavi@cailmobility.com>
2015-04-29 12:07:43 -0600

    added socket.io stuff

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ var dat = require("dat");
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
+var bodyParser = require("body-parser");
+var io = require('socket.io')(http);
+
+app.use(bodyParser.text({type: "*/*"}))
 
 var db = dat("/Users/peyman/Desktop/Database", {}, function(err) {
 
@@ -49,28 +53,42 @@ var createDatOptions = function(contentType) {
 app.post("/file", function(req, res) {
 
   console.log(req.headers["content-type"]);
-  res.send(createDatOptions(req.headers["content-type"]));
+  console.log(req.body);
+  var datOptions = createDatOptions(req.headers["content-type"]);
+  var writer = db.createWriteStream({format: datOptions});
+  var result = writer.end(req.body);
+  res.send(result);
 
 });
+  
+io.on("connection", function(socket){
+  
+  socket.on("getDataStream", function() {
+    
+    db.createReadStream()
+    .on('data', function (data) {
+      
+      socket.emit("dataStream", data);
+      
+    })
+    .on('error', function (err) {
+      console.log('Oh my!', err)
+    })
+    .on('close', function () {
+      console.log('Stream closed')
+    })
+    .on('end', function () {
+      console.log('Stream closed')
+    })
+    
+  });
+  
+})
 
 http.listen(3000, function() {
 
   console.log("listening on port 3000");
 
 });
-
-db.createReadStream()
-  .on('data', function (data) {
-    console.log(data.key, '=', data.name)
-  })
-  .on('error', function (err) {
-    console.log('Oh my!', err)
-  })
-  .on('close', function () {
-    console.log('Stream closed')
-  })
-  .on('end', function () {
-    console.log('Stream closed')
-  })
 
 });
