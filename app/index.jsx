869c9d20a9588680462bc90222303a722d758ce1
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-05-27 12:23:21 -0600

    Fix eslint errors

--- app/index.jsx
+++ app/index.jsx
@@ -3,7 +3,7 @@ import Footer from "./components/footer/Footer.jsx";
 import DataViewer from "./components/data-viewer/DataViewer.jsx";
 import TemplateViewer from "./components/template-viewer/TemplateViewer.jsx";
 import Visualizer from "./components/visualizer/Visualizer.jsx";
-import DataStore from "./controllers/DataStore.js"
+import DataStore from "./controllers/DataStore.js";
 
 $("#collapse-button").on("click", function() {
   var dataP = $("#data-viewer");
