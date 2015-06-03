09ec5c03f71e334896d34ae8128fabd02b8182d9
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-06-03 16:08:55 -0600

    Componentize data-viewer

--- app/components/data-viewer/DataViewer.jsx
+++ app/components/data-viewer/DataViewer.jsx
@@ -1,15 +1,41 @@
 import FieldItem from "./field-item/FieldItem.jsx";
+import PollingDataStore from "./../../controllers/PollingDataStore.js";
 
 export default class DataViewer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {data: []};
+  }
+
+  componentDidMount() {
+    this.unsubscribe = PollingDataStore.listen((data) => {
+      this.setState({data: data});
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
   render() {
-    return <div className="data-viewer" />;
+    return (
+      <div className="data-viewer">
+        <div className="container-fluid">
+          <button className="btn btn-default spaced-button left">Load</button>
+          <button className="btn btn-default spaced-button right">Connect</button>
+        </div>
+
+        <FieldsList data={this.state.data} />
+      </div>
+    );
   }
 }
 
 class FieldsList extends React.Component {
   render() {
     return (
-      <div>
+      <div id="fields-list">
         <h3>Name</h3>
         <FieldItem name="Field 1" value="50%" />
         <FieldItem name="Field 2" value="50%" />
@@ -19,4 +45,4 @@ class FieldsList extends React.Component {
   }
 }
 
-React.render(<FieldsList />, document.getElementById("fields-list"));
+React.render(<DataViewer />, document.getElementById("data-viewer"));
