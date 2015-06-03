9af6590bb62e79c6db7fafefe70aa7789b6878d9
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-06-03 14:55:21 -0600

    Use es6 syntax for FieldItem

--- app/components/data-viewer/field-item/FieldItem.jsx
+++ app/components/data-viewer/field-item/FieldItem.jsx
@@ -1,19 +1,12 @@
-var FieldItem = React.createClass({
-
-   render: function() {
-      return <div className="field-item container-fluid">
-               <div className="col-md-8">
-                  <b>Field Name</b>
-                  <p className="field-name">%75 </p>
-               </div>
-               <a href="http://www.google.com/" className="col-md-4">
-                 <div className="field-link">
-                     Bind
-                  </div>
-               </a>
-             </div>;
-   }
-
-});
-
-module.exports = FieldItem;
+export default class FieldItem extends React.Component {
+  render() {
+    return (
+      <div className="field-item container-fluid">
+         <div className="col-md-8">
+            <b>Field Name</b>
+            <p className="field-name">75%</p>
+         </div>
+      </div>
+    );
+  }
+}
